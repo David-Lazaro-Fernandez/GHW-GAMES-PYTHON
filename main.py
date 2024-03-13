44a8dd103fc79b6f0cc6ddda66812b83fd153e54
David-Lazaro-Fernandez <davidlazaro20@hotmail.com>
2024-03-12 19:50:26 -0600

    Loading Frame Kinda works?

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Example file showing a basic pygame "game loop"
 import pygame
 from apple import Apple
+import os
 
 # pygame setup
 pygame.init()
@@ -11,6 +12,15 @@ clock = pygame.time.Clock()
 background = pygame.image.load("assets/background field.png")
 background = pygame.transform.scale(background, (300, 500))
 
+#Load character running frames
+character_running_frames = []
+character_running_folder_path = "assets/frames/character_running"
+for filename in os.listdir(character_running_folder_path):
+    if filename.endswith(".gif"):
+        image_path = os.path.join(character_running_folder_path, filename)
+        image = pygame.image.load(image_path)
+        character_running_frames.append(image)
+
 running = True
 dt=0
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
@@ -18,8 +28,13 @@ player_radius = 40  # Define this near where you initialize player_pos
 apple = Apple(screen)
 apple_list = []
 apple_generation_timer = 0
+frame_value = 0
 
-
+def redrawGameWindow(frame):
+    screen.blit(background, (0,0))  # This will draw our background image at (0,0)
+    current_frame = frame_value
+    screen.blit(character_running_frames[current_frame], (player_pos.x / 2, player_pos.y / 2))
+    pygame.display.update() 
 
 while running:
     print(apple_generation_timer)
@@ -34,7 +49,6 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.blit(background, (0, 0))
-
     # RENDER YOUR GAME HERE
     pygame.draw.circle(screen, "red", player_pos, player_radius)
 
@@ -66,4 +80,8 @@ while running:
     # independent physics.
     dt = clock.tick(60) / 1000
     apple_generation_timer += 1
+    frame_value += 1
+    if frame_value == len(character_running_frames) - 1:
+        frame_value = 0
+    redrawGameWindow(frame_value) 
 pygame.quit()
