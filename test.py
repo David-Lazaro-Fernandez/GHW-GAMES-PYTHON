cc3029c9e664ed8f8f721b1630615088d0e3613a
David-Lazaro-Fernandez <davidlazaro20@hotmail.com>
2024-03-12 19:50:35 -0600

    test changes

--- test.py
+++ test.py
@@ -8,5 +8,3 @@ for filename in os.listdir(character_running_folder_path):
         image_path = os.path.join(character_running_folder_path, filename)
         image = pygame.image.load(image_path)
         character_running_frames.append(image)
-
-print(character_running_frames)
